16dbab1492af46a8ffdfac6cab2b18c22d2928cf
Bartek-Pisklak <bartek.pisklak@gmail.com>
2023-04-03 19:45:46 +0200

    Init commit

---
+++ Smakosfera/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Smakosfera.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
