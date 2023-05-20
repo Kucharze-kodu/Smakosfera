9422606419fbf9c6ebaab80879247bf0d8e3ff42
Wojtek <wuka6996@gmail.com>
2023-05-20 19:28:19 +0200

    onlyADD

--- smakosfera_front/src/components/AddRecipe.jsx
+++ smakosfera_front/src/components/AddRecipe.jsx
@@ -1,39 +1,215 @@
-import { styles } from "../style";
 
-const AddRecipe = () => {
+import { styles } from "../style";
+import React, { useState } from 'react';
+
+const DodawaniePrzepisu = () => {
+  
+  const [Nazwa, setNazwa] = useState('');
+  const [skladnik, setskladnik] = useState([{nazwa: '', ilosc: ''}]);
+  
+
+  const handleNazwa = (event) => {        //nazwa
+    setNazwa(event.target.value);
+  };
+
+  const handleSkladnik = (event, index, key) => {
+    const updatedskladnik = [...skladnik];
+    updatedskladnik[index][key] = event.target.value;   // składkik i ilosc
+    setskladnik(updatedskladnik);
+  };
+
+ 
+  const handleDodawanko = () => {
+    setskladnik([...skladnik, { nazwa: '', ilosc: '' }]);         //dodane skladniki 
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // Tu trzeba bekent
+
+    // Zresetuj formularz
+    setNazwa('');
+    setskladnik([{ nazwa: '', ilosc: '' }]);
+    
+  };
+
   return (
-    <>
-      {/* Add Recipe section */}
-      <div className={`${styles.paragraph} p-2 `}>
-        Maecenas eleifend ultricies tempus. Mauris fringilla velit lacus,
-        aliquam tristique tellus aliquam id. Ut pharetra eros scelerisque lorem
-        tincidunt convallis. Sed dictum erat lectus, quis efficitur felis semper
-        in. In euismod dui vel fringilla tincidunt. Aliquam ornare nunc ac nunc
-        finibus, in dapibus lorem convallis. Fusce sodales viverra massa, eget
-        blandit nunc commodo nec. Morbi non semper augue. Suspendisse ut nulla
-        eget est semper dapibus vel ut purus. Nulla vel rhoncus mauris, id
-        tempus odio. Duis a lacus dui. Phasellus feugiat urna volutpat orci
-        aliquet, mollis ornare erat pharetra. Phasellus at tortor purus.
-        Suspendisse pellentesque odio eu mauris ornare, et ultrices dui cursus.
-        Nam congue finibus dolor, vel porta neque imperdiet in. Sed sodales nisi
-        quis laoreet ultricies. Nam luctus volutpat feugiat. Proin eu lacinia
-        ante. Mauris at fermentum diam. Aliquam vehicula dolor quis gravida
-        interdum. Aliquam erat volutpat. Nam consectetur rutrum nulla, interdum
-        pretium enim tincidunt a. Curabitur consectetur nibh id mollis feugiat.
-        Nullam dignissim dolor lacus, a aliquet risus mollis sed. Vestibulum a
-        sapien rhoncus orci dignissim consequat ullamcorper in justo. Duis
-        condimentum scelerisque tellus, et finibus turpis bibendum eu. Quisque
-        cursus libero non massa gravida, pretium dictum mauris maximus. Nullam
-        laoreet varius mauris, vitae tincidunt ipsum pellentesque malesuada.
-        Nunc id nibh convallis, consectetur ex non, ullamcorper metus.
-        Suspendisse non finibus neque, nec gravida erat. Morbi blandit ex leo,
-        nec mattis sapien pellentesque sed. Donec quis rutrum tellus, eget
-        tincidunt augue. Phasellus varius ac urna sit amet dignissim. Sed
-        volutpat sagittis leo, ac dapibus nibh sagittis et. Nulla facilisi.
-        Aenean vitae sodales elit.
-      </div>
-    </>
+    <div className="overflow-auto flex flex-col p-3 items-center w-full h-full justify-center xs:justify-start md:my-10 text-center Hover:text-black">
+    <div className={`${styles.heading2 } hover:text-black"` }>Dodaj Przepis !!</div>
+    
+    <form onSubmit={handleSubmit}>
+      <label className="overflow-auto flex flex-col p-3 items-center w-full h-full justify-center xs:justify-start md:my-1 text-center Hover:text-black">
+        <input type="text" value={Nazwa} onChange={handleNazwa} placeholder="Nazwa przepisu:" />
+      </label>
+
+      <label>
+        {
+        skladnik.map((ingredient, index) => (
+          <div key={index}>
+            <input
+             placeholder="Składnik:"
+              type="text"
+              value={ingredient.nazwa}
+              onChange={(event) => handleSkladnik(event, index,'nazwa')}
+            />
+
+            <input
+             placeholder="Ilosc:"
+              type="text"
+              value={ingredient.quantity}
+              onChange={(event) => handleSkladnik(event, index,'ilosc')}
+            />
+            
+          </div>
+        ))
+        }
+        <button type="button" onClick={handleDodawanko} className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>
+          Dodaj składnik
+        </button>
+      </label>
+
+      <button type="submit" className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>Dodaj przepis</button>
+    </form>
+    </div>
   );
 };
 
+export default DodawaniePrzepisu;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const AddRecipe = () => {
+
+    const[Nazwa,setNazwa] = useState("");
+    const[Skladniki, setSkladniki] = useState([]);
+    const[Nazwa_skladnika,setNazwa_skladnika ] = useState("");
+    const[Ilosc,setIlosc] = useState("");
+
+    const Submit = (xd) => 
+    {
+      xd.preventDefault();
+
+      const przepisik = 
+      {
+        Nazwa_skladnika : Nazwa_skladnika,
+        Ilosc: Ilosc
+      };
+
+      setSkladnik([skladniki, nowySkladnik]);
+      setNazwa_skladnika("");
+      setIlosc("");
+    }
+    return(
+
+        <div className="overflow-auto flex flex-col p-3 items-center w-full h-full justify-center xs:justify-start md:my-10 text-center">
+          <div className={`${styles.heading2}`}>Dodaj Przepis !!</div>
+          <form onSubmit={Submit}>
+          <label>
+            Nazwa dania:
+            <input
+              type="text"
+              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
+              value={Nazwa}
+              onChange={(xd) => setNazwa(xd.target.value)}
+            />
+          </label>
+
+          <label>
+            Nazwa Skladnika:
+            <input
+              type="text"
+              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
+              value={Nazwa_skladnika}
+              onChange={(xd) => setNazwa_skladnika(xd.target.value)}
+            />
+          </label>
+
+          <label>
+            Ilosc Skladnika:
+            <input
+              type="text"
+              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
+              value={Ilosc}
+              onChange={(xd) => setIlosc(xd.target.value)}
+            />
+          </label>
+
+          <button
+              type="submit"
+              className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>
+            
+              Wyślij!
+           </button>
+        </form>
+
+       <h2>Lista składników:</h2>
+        <p>Nazwa dania: {Nazwa}</p>
+
+      <ul>
+        {Skladniki.map((ingredient, index) => (
+          <li key={index}>
+            {ingredient.name} - {ingredient.quantity}
+          </li>
+        ))}
+      </ul>
+
+        </div>
+
+    )
+};
+
 export default AddRecipe;
+*/
