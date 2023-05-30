119e722eab7bb80dc87727c3efd9ada985571689
Wojtek <wuka6996@gmail.com>
2023-05-30 11:30:54 +0200

    Done(check json)

--- smakosfera_front/src/components/AddRecipe.jsx
+++ smakosfera_front/src/components/AddRecipe.jsx
@@ -1,8 +1,9 @@
 
 import { styles } from "../style";
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { urlAddRecipe } from "../endpoints";
 import { urlIngredient } from "../endpoints";
+import axios from 'axios';
 
 const DodawaniePrzepisu = () => {
   
@@ -11,15 +12,34 @@ const DodawaniePrzepisu = () => {
   const [trudnosc, setTrudnosc]= useState('');
   const [opis, setOpis]= useState('');
   const [czas, setCzas]= useState('');
+  const [ingredientList, setIngredientList] = useState([]);
 
   const handleNazwa = (event) => {        //nazwa
     setNazwa(event.target.value);
+  };
+
+  useEffect(() => {
+    fetchIngredients();
+  }, []);
+
+  const fetchIngredients = async() => {
+    try {
+      const response = await axios.get(urlIngredient);
+      setIngredientList(response.data);
+      setskladnik([{ nazwa: '', ilosc: '' }]);
+    } catch (error) {
+      console.error('Błąd podczas pobierania składników:', error);
+    }
   };
 
   const handleSkladnik = (event, index, key) => {
     const updatedskladnik = [...skladnik];
     updatedskladnik[index][key] = event.target.value;   // składkik i ilosc
     setskladnik(updatedskladnik);
+  };
+
+  const handleDodawanko = () => {
+    setskladnik([...skladnik, { nazwa: '', ilosc: '' }]);         //dodane skladniki 
   };
 
   const handleTrudnosc = (event) => {
@@ -38,9 +58,7 @@ const DodawaniePrzepisu = () => {
   };
 
  
-  const handleDodawanko = () => {
-    setskladnik([...skladnik, { nazwa: '', ilosc: '' }]);         //dodane skladniki 
-  };
+  
 
   const handleSubmit = async(event) => {
     event.preventDefault();
@@ -52,13 +70,21 @@ const DodawaniePrzepisu = () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: Nazwa,
+          /*name: Nazwa,
           description: opis,
-          difficulty: trudnosc,
-          preparationTime: czas,
+          difficultyLevelId: 1,
+          preparationTime: 5,                       
           skladnik: skladnik,
+          communityRecipe: true,*/
+
+          "name": "Przykładowy przepis",
+          "description": "Opis przepisu",
+          "difficultyLevelId": 2,
+          "preparationTime": 30,
+          "communityRecipe": true,                                // PRZYKLADKOWE DANE 
+          "skladniki": ["skladnik1", "skladnik2"]
       
-        }),
+        })
       });
       let resJson = await res.json();
 
@@ -69,7 +95,10 @@ const DodawaniePrzepisu = () => {
       
       if(res.status === 200){
         setNazwa('');
-        setskladnik([{ nazwa: '', ilosc: '' }])}
+        setskladnik([{ nazwa: '', ilosc: '' }]);
+        setCzas('');
+        setOpis('');
+        setTrudnosc('');}
       else{
         console.log("Błąd!");
       }
@@ -103,45 +132,46 @@ const DodawaniePrzepisu = () => {
      
       <label>
       <h2 className="text-xl font-bold mb-2">Wybierz trudność przepisu:</h2>
-      <select value={trudnosc} onChange={handleTrudnosc} className="w-48 px-4 py-2 mb-10 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500">
-        <option value="">Wybierz trudność</option>
-        <option value="latwe">Łatwe</option>
-        <option value="srednie">Średnie</option>
-        <option value="trudne">Trudne</option>
+      <select type = "number" value={trudnosc} onChange={handleTrudnosc} className="w-48 px-4 py-2 mb-10 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500">
+        <option value="0">Wybierz trudność</option>
+        <option value="1">Łatwe</option>
+        <option value="2">Średnie</option>
+        <option value="3">Trudne</option>
       </select>
 
-      <input type="text" value={czas} onChange={handleCzas} className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500" placeholder="Czas przygotowania:" />
+      <input type="number" value={czas} onChange={handleCzas} className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500" placeholder="Czas przygotowania:" />
 
       </label>
 
       <label>
-        {
-        skladnik.map((ingredient, index) => (
-          <div key={index}>
-            <input
-             placeholder="Składnik:"
-              type="text"
-              value={ingredient.nazwa}
-              onChange={(event) => handleSkladnik(event, index,'nazwa')}
-              className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
+      {skladnik.map((ingredient, index) => (
+        <div key={index}>
+           <select className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500"
+            value={ingredient.nazwa}
+            onChange={(event) => handleSkladnik(event, index, 'nazwa')}
+          >
+            <option value="">Wybierz składnik</option>
+            {ingredientList.map((option) => (
+              <option key={option.id} value={option.name}>
+                {option.name}
+              </option>
+            ))}
+          </select>
 
-            <input
-             placeholder="Ilosc:"
-              type="text"
-              value={ingredient.quantity}
-              onChange={(event) => handleSkladnik(event, index,'ilosc')}
-              className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-            
-          </div>
-        ))
-        }
-        <button type="button" onClick={handleDodawanko} className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>
-          Dodaj składnik
-        </button>
-      </label>
-
+          <input
+            placeholder="Ilosc:"
+            type="number"
+            value={ingredient.ilosc}
+            onChange={(event) => handleSkladnik(event, index, 'ilosc')}
+            className="w-48 px-4 py-2 mb-2 rounded-full border border-gray-300 focus:outline-none focus:border-blue-500"
+          />
+        </div>
+      ))}
+      <button type="button" onClick={handleDodawanko} className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>
+        Dodaj składnik
+      </button>
+    </label>
+      
       <button type="submit" className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>Dodaj przepis</button>
     </form>
     </div>
@@ -203,86 +233,4 @@ export default DodawaniePrzepisu;
 
 
 
-/*
-const AddRecipe = () => {
 
-    const[Nazwa,setNazwa] = useState("");
-    const[Skladniki, setSkladniki] = useState([]);
-    const[Nazwa_skladnika,setNazwa_skladnika ] = useState("");
-    const[Ilosc,setIlosc] = useState("");
-
-    const Submit = (xd) => 
-    {
-      xd.preventDefault();
-
-      const przepisik = 
-      {
-        Nazwa_skladnika : Nazwa_skladnika,
-        Ilosc: Ilosc
-      };
-
-      setSkladnik([skladniki, nowySkladnik]);
-      setNazwa_skladnika("");
-      setIlosc("");
-    }
-    return(
-
-        <div className="overflow-auto flex flex-col p-3 items-center w-full h-full justify-center xs:justify-start md:my-10 text-center">
-          <div className={`${styles.heading2}`}>Dodaj Przepis !!</div>
-          <form onSubmit={Submit}>
-          <label>
-            Nazwa dania:
-            <input
-              type="text"
-              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
-              value={Nazwa}
-              onChange={(xd) => setNazwa(xd.target.value)}
-            />
-          </label>
-
-          <label>
-            Nazwa Skladnika:
-            <input
-              type="text"
-              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
-              value={Nazwa_skladnika}
-              onChange={(xd) => setNazwa_skladnika(xd.target.value)}
-            />
-          </label>
-
-          <label>
-            Ilosc Skladnika:
-            <input
-              type="text"
-              className="rounded-lg w-full pr-[5.3rem] pl-10 p-2 border-[1px] border-dimWhite hover:border-white"
-              value={Ilosc}
-              onChange={(xd) => setIlosc(xd.target.value)}
-            />
-          </label>
-
-          <button
-              type="submit"
-              className={`${styles.paragraph} p-5 mt-3 sm:min-w-[25%] min-w-[100%] border-[1px] focus:border-white hover:border-white border-dimWhite w-[100%] hover:bg-black bg-black rounded-[15px] `}>
-            
-              Wyślij!
-           </button>
-        </form>
-
-       <h2>Lista składników:</h2>
-        <p>Nazwa dania: {Nazwa}</p>
-
-      <ul>
-        {Skladniki.map((ingredient, index) => (
-          <li key={index}>
-            {ingredient.name} - {ingredient.quantity}
-          </li>
-        ))}
-      </ul>
-
-        </div>
-
-    )
-};
-
-export default AddRecipe;
-*/
