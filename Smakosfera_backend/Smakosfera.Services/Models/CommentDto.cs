a254d413514273d96d86b5d8e62893e23b821cec
Jakub Polski <jakubpolski@gmail.com>
2023-05-30 23:14:32 +0200

    obsługa komentarzy
    chyba działa

---
+++ Smakosfera_backend/Smakosfera.Services/Models/CommentDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smakosfera.Services.Models
+{
+    public class CommentDto
+    {
+        [Required]
+        [MinLength(1)]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int RecipeId { get; set; }
+
+        [Required]
+        public int? CommentBossId { get; set; } = null;
+
+    }
+}
