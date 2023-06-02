7477d3e683af80d7fc234f6cf4d1d3f905ab9a32
Jakub Polski <jakubpolski@gmail.com>
2023-06-02 18:20:59 +0200

    Created dto and interface

---
+++ Smakosfera_backend/Smakosfera.Services/Interfaces/ILikeService.cs
@@ -0,0 +1,18 @@
+﻿using Smakosfera.Services.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smakosfera.Services.Interfaces
+{
+    public interface ILikeService
+    {
+        LikeDto GetLike(int LikeId);
+        IEnumerable<LikeDto> GetAllLikes(int RecipeId);
+        OutputLikeDto GetLikesAmount(int RecipeId);
+        void Add(int RecipeId, int UserId);
+        void Remove(int RecipeId, int UserId);
+    }
+}
