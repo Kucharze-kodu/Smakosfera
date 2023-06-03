3814a83a8df1c718a444775c34a6197171e19d14
Jakub Popiołek <grimurin@gmail.com>
2023-06-03 13:41:50 +0200

    Add authorize to delete method in account controller

--- Smakosfera_backend/Smakosfera.WebAPI/Controllers/AccountController.cs
+++ Smakosfera_backend/Smakosfera.WebAPI/Controllers/AccountController.cs
@@ -36,6 +36,7 @@ namespace Smakosfera.WebAPI.Controllers
         }
 
         [HttpDelete]
+        [Authorize]
         public ActionResult Delete()
         {
             _accountService.Delete();
