161e07f7fce7908cd33bb430981a79cabac9ca63
Wojciech Kowal <169567@o365.prz.edu.pl>
2023-06-05 23:03:01 +0200

    add recipe&ingredient with token done

--- smakosfera_front/src/components/AddIngredient.jsx
+++ smakosfera_front/src/components/AddIngredient.jsx
@@ -1,26 +1,35 @@
 import { styles } from "../style";
 import { useState, useEffect } from "react";
-import { urlAddIngredient } from "../endpoints";
+//import { urlAddIngredient } from "../endpoints";
 import { useAuth } from "./AuthContext";
 
 const AddIngredient = () => {
 
     const [nazwa,setNazwa] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [blad, setblad] = useState(false);
 
     const {isLoggedIn} = useAuth();
+    const {getResJsonToken} = useAuth();
+
+    const handleSkladnik = (event) =>{
+        setNazwa(event.target.value)
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
     
-        try {
-          let res = await fetch(urlAddIngredient, {
+       try {
+          let res = await fetch('https://localhost:7000/api/ingredient', {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
+              Authorization:`Bearer ${getResJsonToken()}`
+              
             },
             body: JSON.stringify({
 
-              name: nazwa,
+              name: nazwa
 
             }),
           });
@@ -33,15 +42,32 @@ const AddIngredient = () => {
     
           document.cookie = `resJson=${resJson}; expires=${expirationDate.toUTCString()}; path=/`;
     
-          if (res.status === 200) {
+          if (res.ok) {
             setNazwa("");
           } else {
             console.log("Błąd!");
+            setNazwa("");
           }
-        } catch (err) {
+          
+        } 
+        catch (err) {
+          if (err instanceof SyntaxError && err.message.includes('Unexpected end of JSON input')) 
+          {
+            console.log('Pominięto błąd parsowania JSON');
+          setNazwa("");
+          setSuccess(true);
+          setblad(false)
           console.log(err);
-        }
-      };
+          }
+          else
+          {
+            console.log(err);
+            setblad(true);
+            setSuccess(false)
+          }
+ 
+      }
+    };
 
 
  return (
@@ -49,16 +75,16 @@ const AddIngredient = () => {
       {isLoggedIn() ? (
         <div>
           <div className={`${styles.heading2} xs:my-0 my-4 text-white `}>
-            Dodaj Przepis
+            Dodaj Składnik
           </div>
           <form onSubmit={handleSubmit}>
             <label className="text-center">
               <input
                 type="text"
                 value={nazwa}
-                onChange={handleNazwa}
+                onChange={handleSkladnik}
                 className="w-full px-4 py-2 -++ rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-                placeholder="Nazwa przepisu:"
+                placeholder="Nazwa składnika:"  
               />
             </label>
             <button
@@ -68,6 +94,10 @@ const AddIngredient = () => {
               Dodaj Składnik
             </button>
           </form>
+          <div className={`${styles.heading5} xs:my-0 my-4 text-white `}>
+            {success ? <p>Dodano składnik!</p> : null}
+            {blad ? <p>Coś poszło nie tak </p> : null}
+          </div>
         </div>
       ) : 
       (
