69f9563d8e3a609471562d4037715397c3d10f36
Jakub Popiołek <grimurin@gmail.com>
2023-06-09 16:49:55 +0200

    Add swagger to hosting website

--- Smakosfera_backend/Smakosfera.WebAPI/Program.cs
+++ Smakosfera_backend/Smakosfera.WebAPI/Program.cs
@@ -87,11 +87,8 @@ builder.Services.AddHttpContextAccessor();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 var scope = app.Services.CreateScope();
 var seeder = scope.ServiceProvider.GetRequiredService<SmakosferaSeeder>();
