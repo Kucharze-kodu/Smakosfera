ab3ec2a9c8d629f7e539ddf79d4a31843843a1b2
Jakub Popiołek <grimurin@gmail.com>
2023-06-15 16:27:17 +0200

    Update ParametersController.cs

--- Smakosfera_backend/Smakosfera.WebAPI/Controllers/ParametersController.cs
+++ Smakosfera_backend/Smakosfera.WebAPI/Controllers/ParametersController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Smakosfera.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = "Admin")]
     public class ParametersController : ControllerBase
     {
         private readonly IConfiguration _configuration;
